7bbfa8ab66dc4b61b4e1425c8026282c3c19e13f
mwright0481 <95373969+mwright0481@users.noreply.github.com>
2022-02-24 20:38:03 -0500

    server js

--- server.js
+++ server.js
@@ -103,3 +103,74 @@ const promptUser = () => {
         }
   });
 };
+// View All Employees
+const viewAllEmployees = () => {
+    let sql =       `SELECT employee.id, 
+                    employee.first_name, 
+                    employee.last_name, 
+                    role.title, 
+                    department.department_name AS 'department', 
+                    role.salary
+                    FROM employee, role, department 
+                    WHERE department.id = role.department_id 
+                    AND role.id = employee.role_id
+                    ORDER BY employee.id ASC`;
+    connection.promise().query(sql, (error, response) => {
+      if (error) throw error;
+      console.log(chalk.yellow.bold(`====================================================================================`));
+      console.log(`                              ` + chalk.green.bold(`Current Employees:`));
+      console.log(chalk.yellow.bold(`====================================================================================`));
+      console.table(response);
+      console.log(chalk.yellow.bold(`====================================================================================`));
+      promptUser();
+    });
+  };
+  
+  // View all Roles
+  const viewAllRoles = () => {
+    console.log(chalk.yellow.bold(`====================================================================================`));
+    console.log(`                              ` + chalk.green.bold(`Current Employee Roles:`));
+    console.log(chalk.yellow.bold(`====================================================================================`));
+    const sql =     `SELECT role.id, role.title, department.department_name AS department
+                    FROM role
+                    INNER JOIN department ON role.department_id = department.id`;
+    connection.promise().query(sql, (error, response) => {
+      if (error) throw error;
+        response.forEach((role) => {console.log(role.title);});
+        console.log(chalk.yellow.bold(`====================================================================================`));
+        promptUser();
+    });
+  };
+  
+  // View all Departments
+  const viewAllDepartments = () => {
+    const sql =   `SELECT department.id AS id, department.department_name AS department FROM department`; 
+    connection.promise().query(sql, (error, response) => {
+      if (error) throw error;
+      console.log(chalk.yellow.bold(`====================================================================================`));
+      console.log(`                              ` + chalk.green.bold(`All Departments:`));
+      console.log(chalk.yellow.bold(`====================================================================================`));
+      console.table(response);
+      console.log(chalk.yellow.bold(`====================================================================================`));
+      promptUser();
+    });
+  };
+  
+  // View all Employees by Department
+  const viewEmployeesByDepartment = () => {
+    const sql =     `SELECT employee.first_name, 
+                    employee.last_name, 
+                    department.department_name AS department
+                    FROM employee 
+                    LEFT JOIN role ON employee.role_id = role.id 
+                    LEFT JOIN department ON role.department_id = department.id`;
+    connection.query(sql, (error, response) => {
+      if (error) throw error;
+        console.log(chalk.yellow.bold(`====================================================================================`));
+        console.log(`                              ` + chalk.green.bold(`Employees by Department:`));
+        console.log(chalk.yellow.bold(`====================================================================================`));
+        console.table(response);
+        console.log(chalk.yellow.bold(`====================================================================================`));
+        promptUser();
+      });
+  };
